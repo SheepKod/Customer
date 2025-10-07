0ee47f8766d6f44113bf023bf57b0d8e4f9c0715
nino kristesiashvili <kristesiashvilinino@gmail.com>
2025-10-07 18:47:29 +0400

    updated to init

--- Customer.Domain/Models/IndividualCustomer.cs
+++ Customer.Domain/Models/IndividualCustomer.cs
@@ -10,7 +10,7 @@ public class IndividualCustomer
     public Gender Gender { get; init; }
     public string PersonalId { get; init; } = string.Empty;
     public DateTime DateOfBirth { get; init; }
-    public int CityId { get; set; }
+    public int CityId { get; init; }
     public List<PhoneNumber> PhoneNumbers { get; init; } = [];
     public List<Relation> Relations { get; init; } = [];
     public string ImagePath { get; init; } = string.Empty;
