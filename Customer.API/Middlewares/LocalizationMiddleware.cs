e200f3d07bcf27840044db21df6aa955008d8d4a
nino kristesiashvili <kristesiashvilinino@gmail.com>
2025-10-08 22:59:40 +0400

    made it readonly

--- Customer.API/Middlewares/LocalizationMiddleware.cs
+++ Customer.API/Middlewares/LocalizationMiddleware.cs
@@ -1,10 +1,12 @@
+using System.Collections.ObjectModel;
 using System.Globalization;
 
 namespace Customer.API.Middlewares;
 
 public class LocalizationMiddleware(RequestDelegate next, ILogger<LocalizationMiddleware> logger)
 {
-    private static readonly string[] SupportedCultures = { "ka", "en", "ru", "ge" };
+    private IReadOnlySet<string> SupportedCultures => new HashSet<string> { "ka", "en", "ru", "ge" }; 
+
     private const string DefaultCulture = "ka";
     
     public async Task InvokeAsync(HttpContext context)
