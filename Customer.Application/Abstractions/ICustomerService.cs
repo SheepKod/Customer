b4b330e4b34c564711c6427d523c5d37d255f956
nino kristesiashvili <kristesiashvilinino@gmail.com>
2025-10-11 14:55:29 +0400

    added

---
+++ Customer.Application/Abstractions/ICustomerService.cs
@@ -0,0 +1,21 @@
+using Customer.Application.Dtos;
+using Customer.Application.DTOs;
+using Customer.Domain.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Customer.Application.Abstractions;
+
+public interface ICustomerService
+{
+
+    public Task<int> AddCustomer(AddCustomerDTO customer);
+    public Task<int> AddRelation(AddRelationDTO relation);
+    public Task UpdateCustomer(UpdateCustomerDTO updatedCustomerData);
+    public Task DeleteCustomer(int customerId);
+    public Task DeleteRelation(int relationId);
+
+    public Task<PagedResult<IndividualCustomer>> SearchCustomers(CustomerDetailedSearchDTO search, PagingDTO paging);
+    public Task UploadImage(int customerId, IFormFile image);
+    public Task<List<RelationReport>> GetRelationReport(int customerId);
+
+}
